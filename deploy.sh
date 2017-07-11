83bee701cca00034ed581e0c47edfe95b1c314de
Wenbin Xiao <xwb1989@gmail.com>
2017-07-10 21:10:43 -0400

    move scm_breeze installation to after linking dot-files

--- deploy.sh
+++ deploy.sh
@@ -13,10 +13,6 @@ vim +PluginInstall +qall
 curl -L https://raw.github.com/robbyrussell/oh-my-zsh/master/tools/install.sh | sh
 chsh -s /bin/zsh
 
-# insall scm_breeze
-git clone git://github.com/scmbreeze/scm_breeze.git ~/.scm_breeze
-~/.scm_breeze/install.sh
-
 # link the dot files
 ln -s `pwd`/gitignore ~/.gitignore
 ln -s `pwd`/gitconfig ~/.gitconfig
@@ -24,6 +20,10 @@ ln -s `pwd`/vim ~/.vim
 ln -s `pwd`/vimrc ~/.vimrc
 ln -s `pwd`/zshrc ~/.zshrc
 ln -s `pwd`/vrapperrc ~/.vrapperrc
+
+# insall scm_breeze
+git clone git://github.com/scmbreeze/scm_breeze.git ~/.scm_breeze
+~/.scm_breeze/install.sh
 
 # ready to roll!
 source ~/.zshrc
